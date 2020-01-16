6acd07c09794ac79ec3140c4a8c55866565471fb
Matthew Larrubia <mlarrubia@columbushs.com>
2020-01-16 10:21:59 -0500

    9

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,30 +10,40 @@ import './Home.css'
 // 3. In most cases reusable components
 // 4. Focus on one responsibility
 
-const Home = () =>{
+// const Home = () =>{
 
-    const [ message, setMessage ] = useState('Super Super Message')
-    // const messageState = useState('Super Super Message!!')
-    // const message = messageState[0];
-    // const setMessage = messageState[1];
-
-
-    // Its called after comonenet is initialize and when componenet is Updated!!!
-    // Side effect
-    useEffect(() =>{
-        setTimeout(() =>{
-            setMessage('I am updated Message!####')
-        }, 1000)
-    }, [message, ])
+//     const [ message, setMessage ] = useState('Super Super Message');
+//     const [ count, setCount ] = useState(0);
+//     const [test, setTest] = useState(0);
 
 
-    return (
-        <div className="container">
-            <h1>I am Home Page</h1>
-            <p> { message }</p>
-        </div>
-    )
-}
+//     // Its called after comonenet is initialize and when componenet is Updated!!!
+//     // Side effect
+//     useEffect(() =>{ 
+//         setTimeout(() =>{
+//             alert(test)
+//         }, 1000)
+//     },)
+
+//     const increment = () =>{
+//         // setTest(test + 1)
+//         setCount(count + 1);
+//     }
+
+//     const decrement = () =>{
+//         setCount(count - 1)
+//     }
+
+//     return (
+//         <div className="container">
+//             <h1>I am Home Page</h1>
+//             <p> { message }</p>
+//             <button onClick={increment}>Increment</button>
+//             <div className="counter">{ count }</div>
+//             <button onClick={decrement}>Decrement</button>
+//         </div>
+//     )
+// }
 
 
 // Class Component
@@ -42,38 +52,50 @@ const Home = () =>{
 // 3. More boilerplate
 // 4. Access to lifecycle functions
 
-// class Home extends React.Component {
+class Home extends React.Component {
 
-//     constructor(props){
-//         super(props)
-//         this.state = {
-//             message: 'Super Message!!!!'
-//         }
-//     }
-//     state = {
-//         message: 'Super Message!!!!'
-//     }
+    constructor(props){
+        super(props)
+        this.state = {
+            message: 'Super Message!!!!',
+            count: 0
+        }
+    }
+    
 
 
-//     // Lifecycle function
-//     componentDidMount() {
-//         setTimeout(()=>{
-//             this.setState({message: 'I am updated Message'});
-//         }, 1000)
-//     }
+    // Lifecycle function
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({message: 'I am updated Message'});
+        }, 1000)
+    }
 
-//     // lifecycle function
-//     render(){ 
-//         const { message } = this.state
-//         return (
-//             <div className="container">
-//                 <h1>I am Home Page</h1>
-//                 <p>{message}</p>
-//             </div>
-//         )
-//     }
+    increment = () =>{
+        const { count } = this.state
+        this.setState({count: count + 1})
+    }
 
-// }
+    decrement = () =>{
+        const { count } = this.state
+        this.setState({count: count - 1})
+    }
+
+    // lifecycle function
+    render(){ 
+        const { message, count } = this.state
+        return (
+            <div className="container">
+                <h1>I am Home Page</h1>
+                <p> { message }</p>
+                <button onClick={this.increment}>Increment</button>
+                <div className="counter">{ count }</div>
+                <button onClick={this.decrement}>Decrement</button>
+            </div>
+        )
+    }
+
+}
 
 
 
